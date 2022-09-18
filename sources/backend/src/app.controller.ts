2b557b5df44252ea180c0bf900b5078f1053b1f0
milesdonquijote <milesdonquijote@gmail.com>
2022-09-18 09:59:45 +0300

    create token with code and check user with token

--- sources/backend/src/app.controller.ts
+++ sources/backend/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getMain() {
+    return "welcome to the world where there are no users";
   }
+
+  @Get('signin')
+    async makeGet(@Query('code') code:string) {
+        if (code != undefined)
+            return await this.appService.serviceGet(code);
+        return this.getMain();
+    }
 }
